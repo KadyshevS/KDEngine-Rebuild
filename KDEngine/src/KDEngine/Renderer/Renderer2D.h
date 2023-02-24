78746bcb3bf0483f8338dab3f6f9e0ff8bc41a44
KadyshevS <kadyshev74@gmail.com>
2023-02-25 00:17:35 +0500

    Test flappy bird concept (without collisions)

--- KDEngine/src/KDEngine/Renderer/Renderer2D.h
+++ KDEngine/src/KDEngine/Renderer/Renderer2D.h
@@ -13,12 +13,12 @@ namespace KDE
 		static void Init();
 		static void Shutdown();
 
-		static void BeginScene(const OrthographicCamera& camera);
+		static void BeginScene(OrthographicCamera& camera);
 		static void EndScene();
 
-		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color);
-		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color);
-		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D> texture);
-		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D> texture);
+		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, float rotate, const glm::vec4& color);
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, float rotate, const glm::vec4& color);
+		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, float rotate, const Ref<Texture2D> texture);
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, float rotate, const Ref<Texture2D> texture);
 	};
 }
--- Sandbox/src/Sandox2D.h
+++ Sandbox/src/Sandox2D.h
@@ -2,6 +2,8 @@
 
 #include <KDEngine.h>
 
+#include <random>
+
 class Sandbox2D : public KDE::Layer
 {
 public:
@@ -14,13 +16,38 @@ public:
 	virtual void OnImGuiRender() override;
 	virtual void OnEvent(KDE::Event& e) override;
 private:
+	KDE::Ref<KDE::OrthographicCameraController> m_Camera;
+
 	KDE::Ref<KDE::Texture2D> m_Texture;
+	KDE::Ref<KDE::Texture2D> m_BackTexture;
+	KDE::Ref<KDE::Texture2D> m_PipeTexture;
 
-	KDE::Ref<KDE::OrthographicCameraController> m_CameraController;
+	std::mt19937 mt;
+	std::random_device rd;
+	std::uniform_real_distribution<float> rng;
 
-	glm::vec3 m_QuadPosition = { 0.0f, 0.0f, 0.0f };
-	glm::vec2 m_QuadScale = { 1.0f, 1.0f };
-	glm::vec4 u_Color = { 0.0f, 1.0f, 0.0f, 1.0f };
-	float m_QuadMoveSpeed = 5.0f;
-	float m_QuadScaleSpeed = 3.0f;
+	glm::vec3 m_Position = {0.0f, 0.0f, 0.0f};
+	float m_Rotation = 0.0f;
+
+	float m_BirdUpSpeed = 1.0f;
+	float m_BirdDownSpeed = 1.0f;
+	float m_BirdRotSpeed = 360.0f * 2.0f;
+	float m_PipesSpeed = 0.3f;
+
+	float m_BirdRotLimitUp = 1.0f;
+	float m_BirdRotLimitDown = -1.0f;
+
+	float m_DistanceBetweenPipes = 0.8f;
+	float m_PipesGap = 2.0f;
+
+	float m_BirdSpeed = 0.8f;
+
+	bool m_GameOver = false;
+
+	struct Pipe
+	{
+		glm::vec3 position;
+	};
+
+	Pipe pipes[7];
 };
