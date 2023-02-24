9effbc84b259784ef7e3c2401b2250f93de2a95d
KadyshevS <kadyshev74@gmail.com>
2023-02-24 14:04:44 +0500

    Basic profiling window

--- Sandbox/src/Sandox2D.cpp
+++ Sandbox/src/Sandox2D.cpp
@@ -1,16 +1,50 @@
 #include "kdpch.h"
 #include "Sandox2D.h"
 
-Sandbox2D::Sandbox2D() 
+#include <chrono>
+
+template<typename Fn>
+class Timer
+{
+public:
+	Timer(const char* name, Fn&& func)
+		: m_Name(name), m_Func(func), m_Stopped(false),
+		m_StartPoint(std::chrono::high_resolution_clock::now())
+	{}
+	~Timer()
+	{
+		if (!m_Stopped)
+			Stop();
+	}
+
+	void Stop()
+	{
+		auto endTimepoint = std::chrono::high_resolution_clock::now();
+		long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartPoint).time_since_epoch().count();
+		long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+
+		m_Stopped = true;
+		const float duration = (end - start) * 0.001f;
+
+		m_Func({ m_Name, duration });
+	}
+private:
+	const char* m_Name;
+	Fn m_Func;
+	std::chrono::time_point<std::chrono::steady_clock> m_StartPoint;
+	bool m_Stopped;
+};
+
+#define PROFILE_SCOPE(name) Timer timer##__LINE__(name, [&](ProfileResult profileResult) { m_ProfileResults.push_back(profileResult); });
+
+Sandbox2D::Sandbox2D()
 	: Layer("Sandbox 2D"),
-	m_CameraController(std::make_shared<KDE::OrthographicCameraController>(1280.0f / 720.0f, true)),
-	m_ShaderLibrary(new KDE::ShaderLibrary())
+	m_CameraController(std::make_shared<KDE::OrthographicCameraController>(1280.0f / 720.0f, true))
 {}
 
 void Sandbox2D::OnAttach()
 {
 	m_Texture = KDE::Texture2D::Create("assets/textures/default.png");
-	m_KotTexture = KDE::Texture2D::Create("assets/textures/kot.png");
 }
 void Sandbox2D::OnDetach()
 {
@@ -19,33 +53,45 @@ void Sandbox2D::OnDetach()
 
 void Sandbox2D::OnUpdate(KDE::Timestep ts)
 {
-	//	Input
-	if (KDE::Input::IsKeyPressed('W'))
-		m_QuadPosition.y += m_QuadMoveSpeed * ts;
-	else if (KDE::Input::IsKeyPressed('S'))
-		m_QuadPosition.y -= m_QuadMoveSpeed * ts;
-	if (KDE::Input::IsKeyPressed('A'))
-		m_QuadPosition.x -= m_QuadMoveSpeed * ts;
-	else if (KDE::Input::IsKeyPressed('D'))
-		m_QuadPosition.x += m_QuadMoveSpeed * ts;
+//	Timer
+	PROFILE_SCOPE("Sandbox2D OnUpdate");
 
-	if (KDE::Input::IsKeyPressed('Q'))
-		m_QuadScale += m_QuadScaleSpeed * ts;
-	else if (KDE::Input::IsKeyPressed('E'))
-		m_QuadScale -= m_QuadScaleSpeed * ts;
+//	Input
+	{
+		PROFILE_SCOPE("Sandbox2D Input Handling");
+		if (KDE::Input::IsKeyPressed('W'))
+			m_QuadPosition.y += m_QuadMoveSpeed * ts;
+		else if (KDE::Input::IsKeyPressed('S'))
+			m_QuadPosition.y -= m_QuadMoveSpeed * ts;
+		if (KDE::Input::IsKeyPressed('A'))
+			m_QuadPosition.x -= m_QuadMoveSpeed * ts;
+		else if (KDE::Input::IsKeyPressed('D'))
+			m_QuadPosition.x += m_QuadMoveSpeed * ts;
 
-	//	Drawing
-	KDE::RendererCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
-	KDE::RendererCommand::Clear();
+		if (KDE::Input::IsKeyPressed('Q'))
+			m_QuadScale += m_QuadScaleSpeed * ts;
+		else if (KDE::Input::IsKeyPressed('E'))
+			m_QuadScale -= m_QuadScaleSpeed * ts;
+	}
+	
 
-	m_CameraController->OnUpdate(ts);
+//	Drawing
+	{
+		PROFILE_SCOPE("Sandbox2D Renderer Prep");
+		KDE::RendererCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+		KDE::RendererCommand::Clear();
+	}
+	{
+		PROFILE_SCOPE("Sandbox2D Renderer Update");
+		m_CameraController->OnUpdate(ts);
 
-	KDE::Renderer2D::BeginScene(m_CameraController->GetCamera());
+		KDE::Renderer2D::BeginScene(m_CameraController->GetCamera());
 
-	KDE::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 5.0f, 5.0f }, m_Texture);
-	KDE::Renderer2D::DrawQuad(m_QuadPosition, m_QuadScale, {0.2f, 0.8f, 0.3f, 1.0f});
+		KDE::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 5.0f, 5.0f }, m_Texture);
+		KDE::Renderer2D::DrawQuad(m_QuadPosition, m_QuadScale, u_Color);
 
-	KDE::Renderer2D::EndScene();
+		KDE::Renderer2D::EndScene();
+	}
 }
 void Sandbox2D::OnImGuiRender()
 {
@@ -60,8 +106,15 @@ void Sandbox2D::OnImGuiRender()
 	ImGui::ColorEdit3("", glm::value_ptr(u_Color));
 	ImGui::End();
 
-	ImGui::Begin("Statistics");
-	ImGui::TextColored({ 0.2f, 0.8f, 0.3f, 1.0f }, "FPS: %d", (int)ImGui::GetIO().Framerate);
+	ImGui::Begin("Profile Results");
+	for (auto& pf : m_ProfileResults)
+	{
+		char label[50];
+		strcpy(label, pf.Name);
+		strcat(label, " %.3fms");
+		ImGui::TextColored({ 0.8f, 0.2f, 0.3f, 1.0f }, label, pf.Time);
+	}
+	m_ProfileResults.clear();
 	ImGui::End();
 }
 void Sandbox2D::OnEvent(KDE::Event& e)
--- Sandbox/src/Sandox2D.h
+++ Sandbox/src/Sandox2D.h
@@ -14,22 +14,20 @@ public:
 	virtual void OnImGuiRender() override;
 	virtual void OnEvent(KDE::Event& e) override;
 private:
-	KDE::Ref<KDE::ShaderLibrary> m_ShaderLibrary;
-
-	KDE::Ref<KDE::VertexBuffer> m_SQVertexBuffer;
-	KDE::Ref<KDE::IndexBuffer> m_SQIndexBuffer;
-	KDE::Ref<KDE::VertexArray> m_SQVertexArray;
-
 	KDE::Ref<KDE::Texture2D> m_Texture;
-	KDE::Ref<KDE::Texture2D> m_KotTexture;
 
 	KDE::Ref<KDE::OrthographicCameraController> m_CameraController;
 
 	glm::vec3 m_QuadPosition = { 0.0f, 0.0f, 0.0f };
 	glm::vec2 m_QuadScale = { 1.0f, 1.0f };
-
 	glm::vec4 u_Color = { 0.0f, 1.0f, 0.0f, 1.0f };
-
 	float m_QuadMoveSpeed = 5.0f;
 	float m_QuadScaleSpeed = 3.0f;
+
+	struct ProfileResult
+	{
+		const char* Name;
+		float Time;
+	};
+	std::vector<ProfileResult> m_ProfileResults;
 };
