b86baacc3c6ce08e275f0df2171496759bd6942c
KadyshevS <kadyshev74@gmail.com>
2023-03-06 18:19:00 +0500

    Disabled Profiling

--- KDEngine/src/KDEngine/Debug/Instrumentor.h
+++ KDEngine/src/KDEngine/Debug/Instrumentor.h
@@ -203,7 +203,7 @@ namespace KDE {
 	}
 }
 
-#define KD_PROFILE 1
+#define KD_PROFILE 0
 #if KD_PROFILE
 // Resolve which function signature macro will be used. Note that this only
 // is resolved when the (pre)compiler starts, so the syntax highlighting
