f09e53e5208c8df0625fb9cdd76b836b974101fa
KadyshevS <kadyshev74@gmail.com>
2023-03-31 23:12:26 +0500

    Implemented Scane serializing and deserializing

--- KDEngine/src/KDEngine/Scene/SceneSerializer.cpp
+++ KDEngine/src/KDEngine/Scene/SceneSerializer.cpp
@@ -2,14 +2,79 @@
 #include "SceneSerializer.h"
 
 #include "yaml-cpp/yaml.h"
+#include "glm/glm.hpp"
 
 #include "Entity.h"
 #include "Components.h"
 
 #include <fstream>
 
+namespace YAML
+{
+	template<>
+	struct convert<glm::vec3>
+	{
+		static Node encode(const glm::vec3& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.push_back(rhs.z);
+			return node;
+		}
+		static bool decode(const Node& node, glm::vec3& rhs)
+		{
+			if (!node.IsSequence() || node.size() != 3)
+				return false;
+
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			rhs.z = node[2].as<float>();
+			return true;
+		}
+	};
+
+	template<>
+	struct convert<glm::vec4>
+	{
+		static Node encode(const glm::vec4& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.push_back(rhs.z);
+			node.push_back(rhs.w);
+			return node;
+		}
+		static bool decode(const Node& node, glm::vec4& rhs)
+		{
+			if (!node.IsSequence() || node.size() != 4)
+				return false;
+
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			rhs.z = node[2].as<float>();
+			rhs.w = node[3].as<float>();
+			return true;
+		}
+	};
+}
+
 namespace KDE
 {
+	YAML::Emitter& operator << (YAML::Emitter& out, const glm::vec3& value)
+	{
+		out << YAML::Flow;
+		out << YAML::BeginSeq << value.x << value.y << value.z << YAML::EndSeq;
+		return out;
+	}
+	YAML::Emitter& operator << (YAML::Emitter& out, const glm::vec4& value)
+	{
+		out << YAML::Flow;
+		out << YAML::BeginSeq << value.x << value.y << value.z << value.w << YAML::EndSeq;
+		return out;
+	}
+
 	SceneSerializer::SceneSerializer(const Ref<Scene>& scene)
 		: m_Scene(scene)
 	{}
@@ -29,6 +94,52 @@ namespace KDE
 
 			out << YAML::EndMap;
 		}
+		if (ent.HasComponent<TransformComponent>())
+		{
+			out << YAML::Key << "TransformComponent";
+			out << YAML::BeginMap; // TransformComponent
+
+			auto& tc = ent.GetComponent<TransformComponent>();
+			out << YAML::Key << "Translation" << YAML::Value << tc.Translation;
+			out << YAML::Key << "Rotation" << YAML::Value << tc.Rotation;
+			out << YAML::Key << "Scale" << YAML::Value << tc.Scale;
+
+			out << YAML::EndMap; // TransformComponent
+		}
+		if (ent.HasComponent<CameraComponent>())
+		{
+			out << YAML::Key << "CameraComponent";
+			out << YAML::BeginMap; // CameraComponent
+
+			auto& cameraComponent = ent.GetComponent<CameraComponent>();
+			auto& camera = cameraComponent.Camera;
+
+			out << YAML::Key << "Camera" << YAML::Value;
+			out << YAML::BeginMap; // Camera
+			out << YAML::Key << "ProjectionType" << YAML::Value << (int)camera.GetProjectionType();
+			out << YAML::Key << "PerspectiveFOV" << YAML::Value << camera.GetPerspFOV();
+			out << YAML::Key << "PerspectiveNear" << YAML::Value << camera.GetPerspNearClip();
+			out << YAML::Key << "PerspectiveFar" << YAML::Value << camera.GetPerspFarClip();
+			out << YAML::Key << "OrthographicSize" << YAML::Value << camera.GetOrthoSize();
+			out << YAML::Key << "OrthographicNear" << YAML::Value << camera.GetOrthoNearClip();
+			out << YAML::Key << "OrthographicFar" << YAML::Value << camera.GetOrthoFarClip();
+			out << YAML::EndMap; // Camera
+
+			out << YAML::Key << "Primary" << YAML::Value << cameraComponent.Primary;
+			out << YAML::Key << "FixedAspectRatio" << YAML::Value << cameraComponent.FixedAspectRatio;
+
+			out << YAML::EndMap; // CameraComponent
+		}
+		if (ent.HasComponent<SpriteRendererComponent>())
+		{
+			out << YAML::Key << "SpriteRendererComponent";
+			out << YAML::BeginMap; // SpriteRendererComponent
+
+			auto& spriteRendererComponent = ent.GetComponent<SpriteRendererComponent>();
+			out << YAML::Key << "Color" << YAML::Value << spriteRendererComponent.Color;
+
+			out << YAML::EndMap; // SpriteRendererComponent
+		}
 
 		out << YAML::EndMap;
 	}
@@ -41,9 +152,9 @@ namespace KDE
 		out << YAML::Key << "Entities" << YAML::Value << YAML::BeginSeq;
 		
 		m_Scene->m_Registry.each(
-			[&](auto entityID)
+			[&](auto entID)
 			{
-				Entity ent = { entityID, m_Scene.get() };
+				Entity ent = { entID, m_Scene.get() };
 				if (!ent)
 					return;
 
@@ -57,6 +168,73 @@ namespace KDE
 	}
 	bool SceneSerializer::Deserialize(const std::string& src)
 	{
-		return false;
+		std::ifstream stream(src);
+		KD_CORE_ASSERT(stream.is_open(), "Failed to open file {0}", src.c_str());
+
+		std::stringstream strStream;
+		strStream << stream.rdbuf();
+
+		YAML::Node data = YAML::Load(strStream.str());
+		if (!data["Scene"])
+			return false;
+
+		std::string sceneName = data["Scene"].as<std::string>();
+		KD_CORE_TRACE("Deserializing scene: {0}", sceneName);
+
+		auto entities = data["Entities"];
+		if (entities)
+		{
+			for (auto ent : entities)
+			{
+				uint64_t uuid = ent["Entity"].as<uint64_t>();
+
+				std::string name;
+				auto tagComp = ent["TagComponent"];
+				if (tagComp)
+					name = tagComp["Tag"].as<std::string>();
+
+				KD_CORE_TRACE("Deserialized scene {0} with ID = {1}", name, uuid);
+
+				Entity deserEntity = m_Scene->CreateEntity(name);
+
+				auto transformComp = ent["TransformComponent"];
+				if (transformComp)
+				{
+					auto& tc = deserEntity.GetComponent<TransformComponent>();
+					tc.Translation = transformComp["Translation"].as<glm::vec3>();
+					tc.Rotation = transformComp["Rotation"].as<glm::vec3>();
+					tc.Scale = transformComp["Scale"].as<glm::vec3>();
+				}
+
+				auto camComp = ent["CameraComponent"];
+				if (camComp)
+				{
+					auto& cc = deserEntity.AddComponent<CameraComponent>();
+
+					const auto& cameraProps = camComp["Camera"];
+					cc.Camera.SetProjectionType((SceneCamera::ProjectionType)cameraProps["ProjectionType"].as<int>());
+
+					cc.Camera.SetPerspFOV(cameraProps["PerspectiveFOV"].as<float>());
+					cc.Camera.SetPerspNearClip(cameraProps["PerspectiveNear"].as<float>());
+					cc.Camera.SetPerspFarClip(cameraProps["PerspectiveFar"].as<float>());
+
+					cc.Camera.SetOrthoSize(cameraProps["OrthographicSize"].as<float>());
+					cc.Camera.SetOrthoNearClip(cameraProps["OrthographicNear"].as<float>());
+					cc.Camera.SetOrthoFarClip(cameraProps["OrthographicFar"].as<float>());
+
+					cc.Primary = camComp["Primary"].as<bool>();
+					cc.FixedAspectRatio = camComp["FixedAspectRatio"].as<bool>();
+				}
+
+				auto spriteRendererComponent = ent["SpriteRendererComponent"];
+				if (spriteRendererComponent)
+				{
+					auto& src = deserEntity.AddComponent<SpriteRendererComponent>();
+					src.Color = spriteRendererComponent["Color"].as<glm::vec4>();
+				}
+			}
+		}
+
+		return true;
 	}
 }
